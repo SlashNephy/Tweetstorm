840567ade1ea19f68a19dccc2a7602861b28b688
Slash Nephy <slash@nephy.jp>
2018-08-21 13:40:44 +0900

    Improve Fetcher
    - Implement disconnection phase.
    - Add stream type: User Timeline, Mentions Timeline
    - Improve Ratelimit management.
    - Improve error handling.
    - Fix Task duplication.

--- src/main/kotlin/jp/nephy/tweetstorm/Fetcher.kt
+++ src/main/kotlin/jp/nephy/tweetstorm/Fetcher.kt
@@ -1,9 +1,11 @@
 package jp.nephy.tweetstorm
 
+import io.ktor.util.cio.ChannelWriteException
 import jp.nephy.jsonkt.JsonModel
 import jp.nephy.jsonkt.set
 import jp.nephy.jsonkt.string
 import jp.nephy.penicillin.PenicillinClient
+import jp.nephy.penicillin.PenicillinException
 import jp.nephy.penicillin.model.Status
 import jp.nephy.penicillin.request.ListAction
 import jp.nephy.tweetstorm.session.AuthenticatedStream
@@ -20,7 +22,7 @@ class Fetcher {
     private val tasks = CopyOnWriteArrayList<Fetcher.Task>()
 
     fun startIfNotRunning(stream: AuthenticatedStream) {
-        val task = tasks.find { it.account == stream.account }
+        val task = tasks.find { it.account.id == stream.account.id }
         if (task == null) {
             tasks.add(Task(stream.account, stream))
             return
@@ -34,10 +36,10 @@ class Fetcher {
             application(account.ck, account.cs)
             token(account.at, account.ats)
         }
-
         private val streams = CopyOnWriteArrayList<AuthenticatedStream>().also {
             it.add(initialStream)
         }
+
         fun bind(stream: AuthenticatedStream) {
             if (stream !in streams) {
                 streams.add(stream)
@@ -46,12 +48,49 @@ class Fetcher {
             }
         }
 
-        private fun sendAll(model: JsonModel) {
-            streams.forEach { it.send(model) }
+        fun unbind(stream: AuthenticatedStream) {
+            if (stream in streams) {
+                streams.remove(stream)
+                logger.info { "A Stream has been unbind from @${account.sn}." }
+            }
+        }
+
+        private fun sendAll(payload: JsonModel) {
+            for (it in streams) {
+                try {
+                    it.send(payload)
+                } catch (e: ChannelWriteException) {
+                    unbind(it)
+                } catch (e: Exception) {
+                    logger.error(e) { "A Stream failed sending payload. (@${account.sn})" }
+                    continue
+                }
+            }
         }
 
         init {
-            executor.execute { if (account.listId != null) listTimeline() else homeTimeline() }
+            executor.execute {
+                if (account.listId != null) {
+                    listTimeline()
+                } else {
+                    homeTimeline()
+                }
+            }
+            executor.execute {
+                if (account.listId == null) {
+                    return@execute
+                }
+
+                try {
+                    client.list.member(listId = account.listId, userId = account.id).complete()
+                } catch (e: Exception) {
+                    // if list does not contain me
+                    executor.execute { userTimeline() }
+                    executor.execute { mentionTimeline() }
+                    return@execute
+                }
+
+            }
             executor.execute { directMessage() }
             executor.execute { activity() }
             executor.execute { streams.forEach { friends(it) } }
@@ -60,14 +99,26 @@ class Fetcher {
         }
 
         private fun listTimeline() {
-            timeline(1) {
+            timeline(account.listInterval) {
                 client.list.timeline(listId = account.listId, count = 200, sinceId = it, includeRTs = true, includeEntities = true, options = *arrayOf("tweet_mode" to "extended"))
             }
         }
 
         private fun homeTimeline() {
-            timeline(60) {
+            timeline(account.homeInterval) {
                 client.timeline.home(count = 200, sinceId = it, includeEntities = true, options = *arrayOf("tweet_mode" to "extended"))
+            }
+        }
+
+        private fun userTimeline() {
+            timeline(account.userInterval) {
+                client.timeline.user(count = 200, sinceId = it, includeRTs = true, options = *arrayOf("tweet_mode" to "extended"))
+            }
+        }
+
+        private fun mentionTimeline() {
+            timeline(account.mentionInterval) {
+                client.timeline.mention(count = 200, sinceId = it, includeEntities = true, options = *arrayOf("tweet_mode" to "extended"))
             }
         }
 
@@ -95,16 +146,22 @@ class Fetcher {
                     }
 
                     if (timeline.rateLimit.hasLimit) {
-                        logger.debug { "[Timeline] Rate limit: ${timeline.rateLimit.remaining}/${timeline.rateLimit.limit} (${timeline.rateLimit.resetAt})" }
-
                         val duration = Duration.between(Instant.now(), timeline.rateLimit.resetAt!!.toInstant())
-                        if (timeline.rateLimit.remaining!! * sleepSec * 1.0 / duration.seconds < 1) {
-                            logger.warn { "[Timeline] Rate limit: API calls seem to be frequent than expected. Sleep 5 secs." }
-                            TimeUnit.SECONDS.sleep(5)
+                        if (timeline.rateLimit.remaining!! < 2) {
+                            logger.warn { "[Timeline] Rate limit: Mostly exceeded. Sleep ${duration.seconds} secs. (Reset at ${timeline.rateLimit.resetAt})" }
+                            TimeUnit.SECONDS.sleep(duration.seconds)
+                        } else if (timeline.rateLimit.remaining!! * sleepSec.toDouble() / duration.seconds < 1) {
+                            logger.warn { "[Timeline] Rate limit: API calls (/${timeline.request.url().pathSegments().joinToString("/")}) seem to be frequent than expected so consider adjusting `*_timeline_refresh_sec` value in config.json. Sleep 10 secs. (${timeline.rateLimit.remaining}/${timeline.rateLimit.limit}, Reset at ${timeline.rateLimit.resetAt})" }
+                            TimeUnit.SECONDS.sleep(10)
                         }
                     }
                 } catch (e: Exception) {
-                    logger.error(e) { "[Timeline] An error occurred while getting timeline for @${account.sn}." }
+                    // Rate limit exceeded
+                    if (e is PenicillinException && e.error?.code == 88) {
+                        TimeUnit.SECONDS.sleep(10)
+                    } else {
+                        logger.error(e) { "[Timeline] An error occurred while getting timeline for @${account.sn}." }
+                    }
                 } finally {
                     TimeUnit.SECONDS.sleep(sleepSec.toLong())
                 }
@@ -120,7 +177,13 @@ class Fetcher {
         }
 
         private fun friends(target: AuthenticatedStream) {
-            val friends = client.friend.listIds().complete().untilLast().flatMap { it.result.ids }
+            val friends = try {
+                client.friend.listIds().complete().untilLast().flatMap { it.result.ids }
+            } catch (e: Exception) {
+                logger.error(e) { "[Friends] An error occurred while getting friend ids for @${account.sn}." }
+                return
+            }
+
             if (target.stringifyFriendIds) {
                 target.send("friends_str" to friends.map { "$it" })
             } else {
