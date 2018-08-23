af675a46f4991167a34ab96f79ca20b9abecddfb
Slash Nephy <slash@nephy.jp>
2018-08-23 16:55:14 +0900

    Add isAlive flag

--- src/main/kotlin/jp/nephy/tweetstorm/session/Stream.kt
+++ src/main/kotlin/jp/nephy/tweetstorm/session/Stream.kt
@@ -1,53 +1,56 @@
 package jp.nephy.tweetstorm.session
 
 import io.ktor.http.Parameters
+import jp.nephy.tweetstorm.escapeHtml
+import jp.nephy.tweetstorm.escapeUnicode
+import jp.nephy.tweetstorm.toBooleanEasy
+import java.io.IOException
 import java.io.Writer
-import java.util.concurrent.locks.ReentrantLock
-import kotlin.concurrent.withLock
 
 abstract class Stream(private val writer: Writer, query: Parameters) {
     companion object {
         private const val delimiter = "\r\n"
     }
 
-    private val lock = ReentrantLock()
-    private val delimitedByLength = query["delimited"] == "length"
-    val stringifyFriendIds = query["stringify_friend_ids"].orEmpty().toBoolean()
+    private var isAliveBacking = false
+    val isAlive: Boolean
+        get() = isAliveBacking
+
+    private val delimitedByLength = query["delimited"].equals("length", true)
+    val stringifyFriendIds = query["stringify_friend_ids"].orEmpty().toBooleanEasy()
 
     abstract fun handle()
 
+    fun start() {
+        isAliveBacking = true
+        handle()
+    }
+
+    @Synchronized
+    private fun wrapWriter(content: String) {
+        if (!isAlive){
+            return
+        }
+
+        try {
+            writer.write(content)
+            writer.flush()
+        } catch (e: IOException) {
+            isAliveBacking = false
+            throw e
+        }
+    }
+
     fun send(content: String) {
         val text = "${content.trim().escapeHtml().escapeUnicode()}$delimiter"
-        lock.withLock {
-            if (delimitedByLength) {
-                writer.write("${text.length}$delimiter$text")
-                writer.flush()
-            } else {
-                writer.write(text)
-                writer.flush()
-            }
+        if (delimitedByLength) {
+            wrapWriter("${text.length}$delimiter$text")
+        } else {
+            wrapWriter(text)
         }
     }
 
     fun heartbeat() {
-        lock.withLock {
-            writer.write(delimiter)
-            writer.flush()
-        }
+        wrapWriter(delimiter)
     }
-}
-
-private fun String.escapeHtml(): String {
-    return replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;")
-}
-
-private fun String.escapeUnicode(): String {
-    return map {
-        val code = it.toInt()
-        if (code in 0 until 128) {
-            "$it"
-        } else {
-            String.format("\\u%04x", code)
-        }
-    }.joinToString("")
 }
