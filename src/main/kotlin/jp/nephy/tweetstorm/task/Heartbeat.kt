0c25b4380308e604eb87a99682fd5aca38e4a37a
Slash Nephy <slash@nephy.jp>
2018-08-26 11:18:55 +0900

    Task improvement

--- src/main/kotlin/jp/nephy/tweetstorm/task/Heartbeat.kt
+++ src/main/kotlin/jp/nephy/tweetstorm/task/Heartbeat.kt
@@ -3,10 +3,8 @@ package jp.nephy.tweetstorm.task
 import jp.nephy.tweetstorm.TaskManager
 import java.util.concurrent.TimeUnit
 
-class Heartbeat(override val manager: TaskManager): FetchTask<Nothing>() {
-    override fun provide(data: Nothing) {}
-
-    override fun fetch() {
+class Heartbeat(override val manager: TaskManager): FetchTask() {
+    override fun run() {
         while (true) {
             manager.heartbeat()
             TimeUnit.SECONDS.sleep(10)
--- src/main/kotlin/jp/nephy/tweetstorm/task/Timeline.kt
+++ src/main/kotlin/jp/nephy/tweetstorm/task/Timeline.kt
@@ -12,53 +12,47 @@ import java.time.Duration
 import java.time.Instant
 import java.util.concurrent.TimeUnit
 
-abstract class TimelineTask(final override val manager: TaskManager): FetchTask<Status>() {
+abstract class TimelineTask(final override val manager: TaskManager): FetchTask() {
     val timelineOptions = if (manager.account.markVote) {
         arrayOf("include_entities" to "true", "include_rts" to "true", "tweet_mode" to "extended", "include_cards" to "1", "cards_platform" to "iPhone-13")
     } else {
         arrayOf("include_entities" to "true", "include_rts" to "true", "tweet_mode" to "extended")
     }
 
+    private var lastId: Long? = null
     fun timeline(sleepSec: Int, source: (lastId: Long?) -> ListAction<Status>) {
-        var lastId: Long? = null
-        while (manager.streams.isNotEmpty()) {
-            try {
-                val timeline = source(lastId).complete()
-                if (timeline.result.isNotEmpty()) {
-                    if (lastId != null) {
-                        timeline.result.reversed().forEach {
-                            provide(it)
-                        }
-                    }
-
-                    lastId = timeline.result.first().id
-                }
-
-                if (timeline.rateLimit.hasLimit) {
-                    val duration = Duration.between(Instant.now(), timeline.rateLimit.resetAt!!.toInstant())
-                    if (timeline.rateLimit.remaining!! < 2) {
-                        logger.warn { "Rate limit: Mostly exceeded. Sleep ${duration.seconds} secs. (Reset at ${timeline.rateLimit.resetAt})" }
-                        TimeUnit.SECONDS.sleep(duration.seconds)
-                    } else if (timeline.rateLimit.remaining!! * sleepSec.toDouble() / duration.seconds < 1) {
-                        logger.warn { "Rate limit: API calls (/${timeline.request.url().pathSegments().joinToString("/")}) seem to be frequent than expected so consider adjusting `*_timeline_refresh_sec` value in config.json. Sleep 10 secs. (${timeline.rateLimit.remaining}/${timeline.rateLimit.limit}, Reset at ${timeline.rateLimit.resetAt})" }
-                        TimeUnit.SECONDS.sleep(10)
+        try {
+            val timeline = source(lastId).complete()
+            if (timeline.result.isNotEmpty()) {
+                if (lastId != null) {
+                    timeline.result.reversed().forEach {
+                        manager.emit(it.apply { postProcess() })
                     }
                 }
-            } catch (e: Exception) {
-                // Rate limit exceeded
-                if (e is PenicillinException && e.error?.code == 88) {
-                    TimeUnit.SECONDS.sleep(10)
-                } else {
-                    logger.error(e) { "An error occurred while getting timeline." }
-                }
-            } finally {
-                TimeUnit.SECONDS.sleep(sleepSec.toLong())
+
+                lastId = timeline.result.first().id
             }
-        }
-    }
 
-    override fun provide(data: Status) {
-        manager.emit(data.apply { postProcess() })
+            if (timeline.rateLimit.hasLimit) {
+                val duration = Duration.between(Instant.now(), timeline.rateLimit.resetAt!!.toInstant())
+                if (timeline.rateLimit.remaining!! < 2) {
+                    logger.warn { "Rate limit: Mostly exceeded. Sleep ${duration.seconds} secs. (Reset at ${timeline.rateLimit.resetAt})" }
+                    TimeUnit.SECONDS.sleep(duration.seconds)
+                } else if (timeline.rateLimit.remaining!! * sleepSec.toDouble() / duration.seconds < 1) {
+                    logger.warn { "Rate limit: API calls (/${timeline.request.url().pathSegments().joinToString("/")}) seem to be frequent than expected so consider adjusting `*_timeline_refresh_sec` value in config.json. Sleep 10 secs. (${timeline.rateLimit.remaining}/${timeline.rateLimit.limit}, Reset at ${timeline.rateLimit.resetAt})" }
+                    TimeUnit.SECONDS.sleep(10)
+                }
+            }
+        } catch (e: Exception) {
+            // Rate limit exceeded
+            if (e is PenicillinException && e.error?.code == 88) {
+                TimeUnit.SECONDS.sleep(10)
+            } else {
+                logger.error(e) { "An error occurred while getting timeline." }
+            }
+        } finally {
+            TimeUnit.SECONDS.sleep(sleepSec.toLong())
+        }
     }
 
     private fun Status.postProcess() {
@@ -84,7 +78,7 @@ abstract class TimelineTask(final override val manager: TaskManager): FetchTask<
 }
 
 class ListTimeline(manager: TaskManager): TimelineTask(manager) {
-    override fun fetch() {
+    override fun run() {
         timeline(manager.account.listInterval) {
             manager.twitter.list.timeline(listId = manager.account.listId, count = 200, sinceId = it, options = *timelineOptions)
         }
@@ -92,7 +86,7 @@ class ListTimeline(manager: TaskManager): TimelineTask(manager) {
 }
 
 class HomeTimeline(manager: TaskManager): TimelineTask(manager) {
-    override fun fetch() {
+    override fun run() {
         timeline(manager.account.homeInterval) {
             manager.twitter.timeline.home(count = 200, sinceId = it, options = *timelineOptions)
         }
@@ -100,7 +94,7 @@ class HomeTimeline(manager: TaskManager): TimelineTask(manager) {
 }
 
 class UserTimeline(manager: TaskManager): TimelineTask(manager) {
-    override fun fetch() {
+    override fun run() {
         timeline(manager.account.userInterval) {
             manager.twitter.timeline.user(count = 200, sinceId = it, options = *timelineOptions)
         }
@@ -108,7 +102,7 @@ class UserTimeline(manager: TaskManager): TimelineTask(manager) {
 }
 
 class MentionTimeline(manager: TaskManager): TimelineTask(manager) {
-    override fun fetch() {
+    override fun run() {
         timeline(manager.account.mentionInterval) {
             manager.twitter.timeline.mention(count = 200, sinceId = it, options = *timelineOptions)
         }
