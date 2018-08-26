c62ace3d57d13c9c6651dc093c106b2d4fc5c324
Slash Nephy <slash@nephy.jp>
2018-08-26 22:06:10 +0900

    Update stream

--- src/main/kotlin/jp/nephy/tweetstorm/session/AuthenticatedStream.kt
+++ src/main/kotlin/jp/nephy/tweetstorm/session/AuthenticatedStream.kt
@@ -5,7 +5,7 @@ import jp.nephy.tweetstorm.Config
 import jp.nephy.tweetstorm.fetcher
 import java.io.Writer
 
-class AuthenticatedStream(writer: Writer, override val request: ApplicationRequest, override val account: Config.Account): StreamWriter(writer) {
+class AuthenticatedStream(writer: Writer, override val request: ApplicationRequest, val account: Config.Account): StreamWriter(writer) {
     override fun handle() {
         fetcher.start(this)
     }
--- src/main/kotlin/jp/nephy/tweetstorm/session/SampleStream.kt
+++ src/main/kotlin/jp/nephy/tweetstorm/session/SampleStream.kt
@@ -2,21 +2,21 @@ package jp.nephy.tweetstorm.session
 
 import io.ktor.request.ApplicationRequest
 import jp.nephy.jsonkt.JsonKt
-import jp.nephy.tweetstorm.Config
 import jp.nephy.tweetstorm.builder.CustomStatusBuilder
 import java.io.Writer
-import java.util.*
 import java.util.concurrent.TimeUnit
 
-class SampleStream(writer: Writer, override val request: ApplicationRequest, override val account: Config.Account): StreamWriter(writer) {
+class SampleStream(writer: Writer, override val request: ApplicationRequest): StreamWriter(writer) {
     override fun handle() {
-        while (isAlive) {
-            val status = CustomStatusBuilder.new {
-                text { UUID.randomUUID() }
-            }
+        val status = CustomStatusBuilder.new {
+            text { "This is sample stream. Since Tweetstorm could not authenticate you, sample stream has started. Please check your config.json." }
+        }
 
-            send(JsonKt.toJsonString(status))
-            TimeUnit.SECONDS.sleep(3)
+        send(JsonKt.toJsonString(status))
+
+        while (isAlive) {
+            heartbeat()
+            TimeUnit.SECONDS.sleep(10)
         }
     }
 }
