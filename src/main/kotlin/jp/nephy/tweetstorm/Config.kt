5f586926f207871eb82e7d87df0aa8a8eb9460bd
Nep <spica@starry.blue>
2018-08-26 11:18:19 +0900

    Add sync list following option

--- src/main/kotlin/jp/nephy/tweetstorm/Config.kt
+++ src/main/kotlin/jp/nephy/tweetstorm/Config.kt
@@ -1,5 +1,6 @@
 package jp.nephy.tweetstorm
 
+import ch.qos.logback.classic.Level
 import com.google.gson.JsonObject
 import jp.nephy.jsonkt.*
 import java.nio.file.Paths
@@ -17,9 +18,10 @@ class Config(override val json: JsonObject): JsonModel {
         }
     }
 
-    val host by json.byString { "localhost" }
+    val host by json.byString { "127.0.0.1" }
     val port by json.byInt { 8080 }
     val skipAuth by json.byBool("skip_auth") { false }
+    val logLevel by json.byLambda("log_level") { Level.toLevel(toStringOrDefault("info"), Level.INFO)!! }
     val accounts by json.byModelList<Account>()
 
     class Account(override val json: JsonObject): JsonModel {
@@ -34,6 +36,8 @@ class Config(override val json: JsonObject): JsonModel {
         val listId by json.byNullableLong("list_id")
 
         val debug by json.byBool { false }
+        val syncListFollowing by json.byBool("sync_list_following") { false }
+        val syncListIncludeSelf by json.byBool("sync_list_include_self") { true }
         val enableFriends by json.byBool("enable_friends") { true }
         val markVia by json.byBool("mark_via") { false }
         val markVote by json.byBool("mark_vote") { false }
