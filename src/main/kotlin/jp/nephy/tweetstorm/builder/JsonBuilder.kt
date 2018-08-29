6aca4e8aa7cbb9cb86f9f1e4793008fc5d9e5ea6
Nep <spica@starry.blue>
2018-08-29 21:31:28 +0900

    Add json field so that you may modify JsonObject

--- src/main/kotlin/jp/nephy/tweetstorm/builder/JsonBuilder.kt
+++ src/main/kotlin/jp/nephy/tweetstorm/builder/JsonBuilder.kt
@@ -1,7 +1,9 @@
 package jp.nephy.tweetstorm.builder
 
+import com.google.gson.JsonObject
 import jp.nephy.jsonkt.JsonModel
 
 interface JsonBuilder<T: JsonModel> {
+    val json: JsonObject
     fun build(): T
 }
