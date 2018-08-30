d438cc9c932ed9c7c31dbecb3fb96d51d66f76dc
Slash Nephy <slash@nephy.jp>
2018-08-30 12:59:46 +0900

    Fix a bug which SyncList cannot get entire following list or list's members list

--- src/main/kotlin/jp/nephy/tweetstorm/task/SyncList.kt
+++ src/main/kotlin/jp/nephy/tweetstorm/task/SyncList.kt
@@ -8,30 +8,30 @@ import java.util.concurrent.TimeUnit
 class SyncList(override val manager: TaskManager): RegularTask(5, TimeUnit.MINUTES) {
     override fun run() {
         val followingIds = if (manager.account.syncListIncludeSelf) {
-            manager.twitter.friend.listIds().complete().untilLast().allIds + manager.account.id
+            manager.twitter.friend.listIds(count = 5000).complete().untilLast().allIds + manager.account.id
         } else {
-            manager.twitter.friend.listIds().complete().untilLast().allIds
+            manager.twitter.friend.listIds(count = 5000).complete().untilLast().allIds
         }
 
-        if (followingIds.size > 3000) {
-            logger.warn { "This list exceeded 3000 members limit." }
+        if (followingIds.size > 5000) {
+            logger.warn { "This list exceeded 5000 members limit." }
             return
         }
 
-        val listMemberIds = manager.twitter.list.members(listId = manager.account.listId).complete().untilLast().allUsers.map { it.id }
+        val listMemberIds = manager.twitter.list.members(listId = manager.account.listId, count = 5000).complete().untilLast().allUsers.map { it.id }
 
-        val willBeRemoved = listMemberIds.filter { it !in followingIds }
+        val willBeRemoved = listMemberIds - followingIds
         if (willBeRemoved.isNotEmpty()) {
             willBeRemoved.chunked(100).forEach {
-                manager.twitter.list.removeMembers(listId = manager.account.listId, userIds = it).queue()
+                manager.twitter.list.removeMembers(listId = manager.account.listId, userIds = it).complete()
             }
             logger.debug { "Removing ${willBeRemoved.size} user(s)." }
         }
 
-        val willBeAdded = followingIds.filter { it !in listMemberIds }
+        val willBeAdded = followingIds - listMemberIds
         if (willBeAdded.isNotEmpty()) {
             willBeAdded.chunked(100).forEach {
-                manager.twitter.list.addMembers(listId = manager.account.listId, userIds = it).queue()
+                manager.twitter.list.addMembers(listId = manager.account.listId, userIds = it).complete()
             }
             logger.debug { "Adding ${willBeAdded.size} user(s)." }
         }
