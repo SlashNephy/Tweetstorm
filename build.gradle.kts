e0e648a62befc67602f5bf742bdb419e44372885
Slash Nephy <slash@nephy.jp>
2018-09-01 17:26:39 +0900

    Update ktor to 0.9.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.jetbrains.kotlin.gradle.dsl.Coroutines
 
-val ktorVersion = "0.9.4-alpha-2"
+val ktorVersion = "0.9.4"
 
 plugins {
     kotlin("jvm") version "1.2.61"
@@ -27,10 +27,11 @@ dependencies {
     compile("io.ktor:ktor-server-netty:$ktorVersion")
     compile("io.ktor:ktor-html-builder:$ktorVersion")
     compile("org.jetbrains.kotlinx:kotlinx-html-jvm:0.6.11")
+    compile("org.jetbrains.kotlinx:atomicfu:0.11.3")
 
     compile("jp.nephy:penicillin:3.0.0")
 
-    compile("io.github.microutils:kotlin-logging:1.4.9")
+    compile("io.github.microutils:kotlin-logging:1.5.9")
     compile("ch.qos.logback:logback-core:1.2.3")
     compile("ch.qos.logback:logback-classic:1.2.3")
     compile("org.fusesource.jansi:jansi:1.17.1")
