e3f7c93a5e763c62f2d84d490c6cfd3b1a3482e6
Nep <spica@starry.blue>
2018-09-03 01:35:18 +0900

    Add DM support

--- src/main/kotlin/jp/nephy/tweetstorm/Config.kt
+++ src/main/kotlin/jp/nephy/tweetstorm/Config.kt
@@ -44,6 +44,7 @@ class Config(override val json: JsonObject): JsonModel {
 
         val syncListFollowing by json.byBool("sync_list_following") { false }
         val syncListIncludeSelf by json.byBool("sync_list_include_self") { true }
+        val enableDirectMessage by json.byBool("enable_direct_message") { true }
         val enableFriends by json.byBool("enable_friends") { true }
         val markVia by json.byBool("mark_via") { false }
         val markVote by json.byBool("mark_vote") { false }
@@ -51,5 +52,6 @@ class Config(override val json: JsonObject): JsonModel {
         val homeInterval by json.byInt("home_timeline_refresh_sec") { 90 }
         val userInterval by json.byInt("user_timeline_refresh_sec") { 3 }
         val mentionInterval by json.byInt("mention_timeline_refresh_sec") { 45 }
+        val messageInterval by json.byInt("direct_message_refresh_sec") { 90 }
     }
 }
