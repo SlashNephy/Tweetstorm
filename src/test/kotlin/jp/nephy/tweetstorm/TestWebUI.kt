27ee6589c0f00002e16880bb31b34f469d04fa34
Slash Nephy <slash@nephy.jp>
2018-09-07 10:50:56 +0900

    Add test for WUI

---
+++ src/test/kotlin/jp/nephy/tweetstorm/TestWebUI.kt
@@ -0,0 +1,19 @@
+package jp.nephy.tweetstorm
+
+import io.ktor.application.install
+import io.ktor.features.CallLogging
+import io.ktor.routing.Routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+
+private const val host = "localhost"
+private const val port = 8080
+
+fun main(args: Array<String>) {
+    embeddedServer(Netty, host = host, port = port) {
+        install(CallLogging)
+        install(Routing) {
+            getTop()
+        }
+    }.start(wait = true)
+}
