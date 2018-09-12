6900e2ad0cf6bbcd8a065a55be159dd5ef0c079b
Nep <spica@starry.blue>
2018-09-12 22:07:00 +0900

    Friends ids are cached on Config

--- src/main/kotlin/jp/nephy/tweetstorm/task/producer/Friends.kt
+++ src/main/kotlin/jp/nephy/tweetstorm/task/producer/Friends.kt
@@ -1,6 +1,5 @@
 package jp.nephy.tweetstorm.task.producer
 
-import jp.nephy.penicillin.core.allIds
 import jp.nephy.tweetstorm.session.AuthenticatedStream
 import jp.nephy.tweetstorm.task.TargetedProduceTask
 import jp.nephy.tweetstorm.task.data.JsonData
@@ -12,12 +11,11 @@ import kotlin.coroutines.experimental.CoroutineContext
 class Friends(private val target: AuthenticatedStream): TargetedProduceTask<JsonData>(target) {
     override fun channel(context: CoroutineContext, parent: Job) = produce(context, parent = parent) {
         val stringifyFriendIds = target.request.queryParameters["stringify_friend_ids"].orEmpty().toBooleanEasy()
-        val data = target.account.twitter.friend.listIds().await().untilLast().allIds
 
         if (stringifyFriendIds) {
-            send(JsonData("friends_str" to data.map { "$it" }))
+            send(JsonData("friends_str" to account.friends.map { "$it" }))
         } else {
-            send(JsonData("friends" to data))
+            send(JsonData("friends" to account.friends))
         }
     }
 }
