1326ebca59d105c5dd3bfd89715cd002e8deeed5
Nep <spica@starry.blue>
2018-09-13 21:47:48 +0900

    Fix PreAuthenticatedStream could not exit by timeout

--- src/main/kotlin/jp/nephy/tweetstorm/session/PreAuthenticatedStream.kt
+++ src/main/kotlin/jp/nephy/tweetstorm/session/PreAuthenticatedStream.kt
@@ -2,14 +2,10 @@ package jp.nephy.tweetstorm.session
 
 import io.ktor.features.origin
 import io.ktor.request.ApplicationRequest
-import jp.nephy.jsonkt.jsonArray
 import jp.nephy.tweetstorm.Config
 import jp.nephy.tweetstorm.builder.newStatus
-import jp.nephy.tweetstorm.toBooleanEasy
 import kotlinx.coroutines.experimental.delay
 import kotlinx.coroutines.experimental.io.ByteWriteChannel
-import java.io.IOException
-import java.io.Writer
 import java.util.*
 import java.util.concurrent.CopyOnWriteArrayList
 import java.util.concurrent.TimeUnit
@@ -48,39 +44,25 @@ class PreAuthenticatedStream(channel: ByteWriteChannel, request: ApplicationRequ
 
         register(this)
 
-        try {
-            val stringifyFriendIds = request.queryParameters["stringify_friend_ids"].orEmpty().toBooleanEasy()
-            if (stringifyFriendIds) {
-                handler.emit("friends_str" to jsonArray())
-            } else {
-                handler.emit("friends" to jsonArray())
+        handler.emit(newStatus {
+            user {
+                name("Tweetstorm Authenticator")
+            }
+            text { "To start streaming, access https://userstream.twitter.com/auth/token/$urlToken" }
+            url("https://userstream.twitter.com/auth/token/$urlToken", 27, 101)
+        })
+
+        repeat(300) {
+            if (!contain(this)) {
+                return true
+            }
+            else if (it % 10 == 0 && !handler.heartbeat()) {
+                return false
             }
 
-            delay(3, TimeUnit.SECONDS)
-
-            handler.heartbeat()
-
-            handler.emit(newStatus {
-                user {
-                    name("Tweetstorm Authenticator")
-                }
-                text { "To start streaming, access https://userstream.twitter.com/auth/token/$urlToken" }
-                url("https://userstream.twitter.com/auth/token/$urlToken", 27, 101)
-            })
-
-            repeat(300) {
-                if (!contain(this)) {
-                    return true
-                }
-
-                if (it % 10 == 0) {
-                    handler.heartbeat()
-                }
-                delay(1, TimeUnit.SECONDS)
-            }
-            return false
-        } catch (e: IOException) {
-            return false
+            delay(1, TimeUnit.SECONDS)
         }
+
+        return false
     }
 }
